028461974172329ea985b0230a2776fcf4ad2c8d
Sahil <sahil06202@gmail.com>
2024-04-11 10:33:56 +0530

    sixth commit

---
+++ LinkedList/appendlastk.cpp
@@ -0,0 +1,108 @@
+#include <iostream>
+using namespace std;
+
+class node
+{
+public:
+    int data;
+    node *next;
+
+    node(int val)
+    {
+        data = val;
+        next = NULL;
+    }
+};
+
+void insertAtTail(node *&head, int val)
+{
+
+    node *n = new node(val);
+    if (head == NULL)
+    {
+        head = n;
+        return;
+    }
+    node *temp = head;
+    while (temp->next != NULL)
+    {
+        temp = temp->next;
+    }
+    temp->next = n;
+}
+
+void insertAtHead(node *&head, int val)
+{
+    node *n = new node(val);
+    n->next = head;
+    head = n;
+}
+
+void display(node *head)
+{
+    node *temp = head;
+    while (temp != NULL)
+    {
+        cout << temp->data << "->";
+        temp = temp->next;
+    }
+    cout << "NULL";
+}
+
+int length(node *head)
+{
+    int l = 0;
+    node *temp = head;
+    while (temp != NULL)
+    {
+        l++;
+        temp = temp->next;
+    }
+    return l;
+}
+
+node *kappend(node *&head, int k)
+{
+    node *newHead;
+    node *newTail;
+    node *tail = head;
+
+    int l = length(head);
+    k = k % l;
+    int count = 1;
+
+    while (tail->next != NULL)
+    {
+        if (count == l - k)
+        {
+            newTail = tail;
+        }
+        if (count == l - k + 1)
+        {
+            newHead = tail;
+        }
+        tail = tail->next;
+        count++;
+    }
+    newTail->next = NULL;
+    tail->next = head;
+
+    return newHead;
+}
+
+int main()
+{
+    node *head = NULL;
+    insertAtTail(head, 1);
+    insertAtTail(head, 2);
+    insertAtTail(head, 3);
+    insertAtTail(head, 4);
+    insertAtTail(head, 5);
+    insertAtTail(head, 6);
+    display(head);
+    cout << endl;
+    node *newHead = kappend(head, 4);
+    display(newHead);
+
+    return 0;
+}
---
+++ LinkedList/ll.cpp
@@ -0,0 +1,217 @@
+#include <iostream>
+using namespace std;
+
+class node
+{
+public:
+    int data;
+    node *next;
+
+    node(int val)
+    {
+        data = val;
+        next = NULL;
+    }
+};
+
+void insertAtTail(node *&head, int val)
+{
+    node *n = new node(val);
+
+    if (head == NULL)
+    {
+        head = n;
+        return;
+    }
+
+    node *temp = head;
+    while (temp->next != NULL)
+    {
+        temp = temp->next;
+    }
+    temp->next = n;
+}
+
+void insertAtHead(node *&head, int val)
+{
+    node *n = new node(val);
+    n->next = head;
+    head = n;
+}
+
+bool search(node *head, int key)
+{
+    node *temp = head;
+    while (temp != NULL)
+    {
+        if (temp->data == key)
+        {
+            return true;
+        }
+        temp = temp->next;
+    }
+    return false;
+}
+
+void display(node *head)
+{
+    node *temp = head;
+    while (temp != NULL)
+    {
+        cout << temp->data << "->";
+        temp = temp->next;
+    }
+    cout << "NULL";
+}
+
+void deleteHeadNode(node *&head)
+{
+    node *todelete = head;
+    head = head->next;
+    delete todelete;
+}
+
+void deletion(node *&head, int val)
+{
+    if (head == NULL)
+    {
+        return;
+    }
+
+    if (head->next == NULL)
+    {
+        deleteHeadNode(head);
+        return;
+    }
+
+    node *temp = head;
+    while (temp->next->data != val)
+    {
+        temp = temp->next;
+    }
+    node *todelete = temp->next;
+    temp->next = temp->next->next;
+
+    delete todelete;
+}
+
+node *reverse(node *&head)
+{
+    node *prevptr = NULL;
+    node *currptr = head;
+    node *nextptr;
+
+    while (currptr != NULL)
+    {
+        nextptr = currptr->next;
+        currptr->next = prevptr;
+
+        prevptr = currptr;
+        currptr = nextptr;
+    }
+    // head = prevptr;
+    return prevptr;
+}
+
+node *reversek(node *&head, int k)
+{
+    node *prevptr = NULL;
+    node *currptr = head;
+    node *nextptr;
+
+    int count = 0;
+
+    while (currptr != NULL && count < k)
+    {
+        nextptr = currptr->next;
+        currptr->next = prevptr;
+        prevptr = currptr;
+        currptr = nextptr;
+        count++;
+    }
+
+    if (nextptr != NULL)
+    {
+        head->next = reversek(nextptr, k);
+    }
+    return prevptr;
+}
+
+bool detectCycle(node *&head)
+{
+    node *slow = head;
+    node *fast = head;
+
+    while (fast != NULL && fast->next != NULL)
+    {
+        slow = slow->next;
+        fast = fast->next->next;
+
+        if (fast == slow)
+        {
+            return true;
+        }
+    }
+    return false;
+}
+
+void makeCycle(node *&head, int pos)
+{
+    node *temp = head;
+    node *startNode;
+
+    int count = 1;
+    while (temp->next != NULL)
+    {
+        if (count == pos)
+        {
+            startNode = temp;
+        }
+        temp = temp->next;
+        count++;
+    }
+    temp->next = startNode;
+}
+
+int main()
+{
+
+    node *head = NULL;
+    insertAtTail(head, 1);
+    insertAtTail(head, 2);
+    insertAtTail(head, 3);
+    insertAtTail(head, 4);
+    insertAtHead(head, 5);
+
+    display(head);
+
+    deletion(head, 3);
+    cout << endl;
+    display(head);
+
+    deleteHeadNode(head);
+    cout << endl;
+    display(head);
+    cout << endl;
+
+    head = reverse(head);
+    display(head);
+    cout << endl;
+    insertAtTail(head, 7);
+    display(head);
+    cout << endl;
+    int k = 2;
+    node *newhead = reversek(head, k);
+    display(newhead);
+
+    cout << endl;
+    cout << search(head, 5);
+    cout << endl;
+    // Detect cycle
+    insertAtTail(head, 9);
+    insertAtTail(head, 12);
+    makeCycle(head, 3);
+    cout << detectCycle(head) << endl;
+
+    return 0;
+}
